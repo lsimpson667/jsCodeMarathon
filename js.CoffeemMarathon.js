2405033e62f49d34a0872e68a3e884fbed8dbba8
Lois Simpson <lsimpson667@gmail.com>
2020-03-02 22:02:56 -0500

    added html to create receipt

---
+++ js.CoffeemMarathon.js
@@ -0,0 +1,34 @@
+console.log("Coffee Please!");
+let coffeeType = "Medium Roast";
+console.log(coffeeType);
+
+var latte = 2;
+var mocha = 3;
+// console.log(latte + mocha);
+// console.log(latte > mocha);
+// console.log(latte<mocha);
+
+// let sugarfree;
+// alert(sugarfree);
+
+let customer1 = "Kamal Khan"; let drinkChoice = "Chai", quantity = 4, foodAllergy = "false";
+// console.log(customer1 + " " + drinkChoice + " " + quantity +" " + foodAllergy);
+
+// console.log(`${customer1} ${drinkChoice} ${quantity} ${foodAllergy}`);
+
+let chai = 2.50;
+// console.log(chai*quantity);
+
+// let chai = 2;
+// var chai = 2;
+
+// console.log(chai);
+console.log("+++++++++++++++++++++++++++++++++++++++++++++++++");
+let total = "Your Final Order is " + quantity + " " + drinkChoice + " " + "for a total  of $" + quantity * chai + ".";
+
+console.log(total);
+console.log("++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+console.log("Nice choice of " + drinkChoice + ", but we recommend Sprite!");
+
+// let total = (`Your Final Order is ${quantity} ${drinkChoice} for a total  of $ ${quantity} * chai + ".";`)
